828b4a1eb4267eecf123b050a583f0f14a95ad81
Yogesh <52989607+yogesh2k21@users.noreply.github.com>
2022-10-15 15:52:15 +0530

    dubug false
    dubug false and allowed host all

--- fyle_classroom/settings.py
+++ fyle_classroom/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'r#g(!ab-ca(_m()v!)+3q52iuqvomvcc2@b+)(&wg0y*^b7i+q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
